18a44dd0ca23de821b5572d41e919bdb4bf77e3d
ticze <ticze2598@gmail.com>
2018-10-30 11:36:40 -0500

    configuracion firebase y boilerplate

---
+++ src/controller/index.js
@@ -0,0 +1,16 @@
+const btnReserva = document.getElementById("btnreserva");
+btnReserva.addEventListener("click", e => {
+  e.preventDefault();
+  const agencia = document.getElementById("nombreAgencia").value;
+  const producto = document.getElementById("producto").value;
+  const fecha = document.getElementById("fecha").value;
+  const hora = document.getElementById("hora").value;
+  // Get a reference to the database service
+  const datosProducto = firebase.database().ref().child('producto');
+  datosProducto.push({
+    nombre_agencia: agencia,
+    producto: producto,
+    fecha: fecha,
+    hora: hora
+  })
+});
---
+++ src/model/call-firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCewaF69yg8E-nNNgklppFXA63SCUp_76s",
+  authDomain: "reservationtv-951e1.firebaseapp.com",
+  databaseURL: "https://reservationtv-951e1.firebaseio.com",
+  projectId: "reservationtv-951e1",
+  storageBucket: "reservationtv-951e1.appspot.com",
+  messagingSenderId: "640610793868"
+};
+firebase.initializeApp(config);
