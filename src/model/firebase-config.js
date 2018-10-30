71c9bd399a0df5f39f431dded777da188c896e87
yulissateran <yulissa.lteran@gmail.com>
2018-10-30 12:29:58 -0500

    add modelado de datos de horario de publicidad

---
+++ src/model/firebase-config.js
@@ -0,0 +1,305 @@
+/*eslint-disable */
+
+/* global firebase */
+var config = {
+  apiKey: "AIzaSyCewaF69yg8E-nNNgklppFXA63SCUp_76s",
+  authDomain: "reservationtv-951e1.firebaseapp.com",
+  databaseURL: "https://reservationtv-951e1.firebaseio.com",
+  projectId: "reservationtv-951e1",
+  storageBucket: "reservationtv-951e1.appspot.com",
+  messagingSenderId: "640610793868"
+};
+firebase.initializeApp(config);
+window.database = firebase.database();
+window.database.ref('horarios').set({
+  horas: [
+    {
+      hora: 05,
+      publicidad: [
+        {
+          intervalo: '5:16-5:30',
+          duración: 840,
+          disponibles: 840,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+    {
+      hora: ,
+      publicidad: [
+        {
+          intervalo: '',
+          duración: ,
+          disponibles: ,
+          reservados: 0,
+          status: 'disponible'
+        },
+       ],
+    },
+  ]
+})
