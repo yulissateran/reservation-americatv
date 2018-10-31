73d44084fc083dfbbf79b11aacce1a008090bd49
ticze <ticze2598@gmail.com>
2018-10-31 00:14:42 -0500

    data de programacion

--- src/model/firebase-config.js
+++ src/model/firebase-config.js
@@ -1,19 +1,626 @@
-
-
-window.database.ref('horarios').set({
-  horas: [
+window.database.ref("programación/martes").set({
+  programming: [
     {
-      hora: 05,
-      publicidad: [
-        {
-          intervalo: '5:16-5:30',
-          duración: 840,
-          disponibles: 840,
-          reservados: 0,
-          status: 'disponible'
-        },
-       ],
+      monto: 1500,
+      recarga:1725,
+      programa: "Mujeres de Negro(novela)",
+      publicidad: ["0030-00:35", "1:00-1:05"],
+      time: "00:30 - 01:30"
     },
-    
+    {
+      recarga:1380,
+      monto: 1200,
+      programa: "Sin tu Mirada",
+      publicidad: ["1:30-1:35", "2:00-2:05"],
+      time: "01:30 - 02:30"
+    },
+    {
+      recarga:3450,
+      monto: 3000,
+      programa: "En Boca de Todos (repeticion)",
+      publicidad: ["2:30-2:35", "3:00-3:05"],
+      time: "02:30 - 03:30"
+    },
+    {
+      recarga:1150,
+      monto: 1000,
+      programa: "El Privilegio de Amar",
+      publicidad: ["3:30-3:35", "4:00-4:05"],
+      time: "03:30 - 04:30"
+    },
+    {
+      recarga:2875,
+      monto: 2500,
+      programa: "América Espectáculos (repeticion)",
+      publicidad: ["4:30-4:35", "5:00-5:05"],
+      time: "04:30 - 05:30"
+    },
+    {
+      recarga:3450,
+      monto: 3000,
+      programa: "America Noticias: Primera Edición: Local",
+      publicidad: ["5:30-5:35", "6:00-6:05", "6:30-6:35"],
+      time: "5:30 - 07:00"
+    },
+    {
+      recarga:3450,
+      monto: 3000,
+      programa: "América Noticias Primera Edición",
+      publicidad: ["7:00-5:05", "7:30-7:35"],
+      time: "07:00 - 08:00"
+    },
+    {
+      recarga:1000,
+      monto: 1000,
+      programa: "América Deportes",
+      publicidad: ["8:00-8:05"],
+      time: "08:00 - 08:20"
+    },
+    {
+      recarga:3000,
+      monto: 3000,
+      programa: "América Noticias Primera Edición",
+      publicidad: ["8:30-8:35"],
+      time: "08:20 - 09:00"
+    },
+    {
+      recarga:1000,
+      monto: 1000,
+      programa: "El Privilegio de Amar(novela)",
+      publicidad: ["9:00-9:05", "9:30-9:35"],
+      time: "09:00 - 10:00"
+    },
+    {
+      recarga:1000,
+      monto: 1000,
+      programa: "Marimar(novela)",
+      publicidad: ["10:00-10:05"],
+      time: "10:00 - 10:15"
+    },
+    {
+      recarga:1000,
+      monto: 1000,
+      programa: "Los Amores de Polo",
+      publicidad: ["11:00-11:05", "11:30-11:35"],
+      time: "11:00 - 12:00"
+    },
+    {
+      recarga:4600,
+      monto: 4000,
+      programa: "America Noticias: Edición del Mediodia",
+      publicidad: ["12:00-12:05", "12:30-12:35"],
+      time: "12:00 - 01:00"
+    },
+    {
+      recarga:3450,
+      monto: 3000,
+      programa: "En Boca de Todos",
+      publicidad: ["13:00-13:05", "13:30-13:35", "14:00-14:05"],
+      time: "13:00 - 14:20"
+    },
+    {
+      recarga:2300,
+      monto: 2200,
+      programa: "Al Fondo Hay Sitio",
+      publicidad: ["14:30-14:35", "15:00-15:05", "15:30-15:35"],
+      time: "14:20 - 16:00"
+    },
+    {
+      recarga:1150,
+      monto: 1000,
+      programa: "Caer en Tentación",
+      publicidad: ["16:00-16:05", "16:30-16:35"],
+      time: "16:00 - 17:00"
+    },
+    {
+      recarga:1380,
+      monto: 1200,
+      programa: "La Rosa de Guadalupe",
+      publicidad: ["17:00-17:05", "17:30-17:35", "18:00-18:05"],
+      time: "17:00 - 18:20"
+    },
+    {
+      recarga:1380,      
+      monto: 1200,
+      programa: "Cumbia Pop",
+      publicidad: ["18:30-18:35"],
+      time: "18:20 - 19:00"
+    },
+    {
+      recarga:5750,
+      monto: 5000,
+      programa: "Esto es Guerra",
+      publicidad: ["19:00-19:05", "19:30-19:35"],
+      time: "19:00 - 20:00"
+    },
+    {
+      recarga:1380,
+      monto: 1200,
+      programa: "VBQ: Empezando a Vivir",
+      publicidad: ["20:00-20:05", "20:30-20:35"],
+      time: "20:00 - 21:00"
+    },
+    {
+      recarga:1725,
+      monto: 1500,
+      programa: "Ojitos Hechiceros",
+      publicidad: ["21:00-21:05", "21:30-21:35"],
+      time: "21:00 - 22:00"
+    },
+    {
+      recarga:4600,
+      monto: 4000,
+      programa: "America Noticias: Edicion Central",
+      publicidad: ["22:00-22:05", "22:30-22:35", "23:00-23:05"],
+      time: "22:00 - 23:30"
+    },
+    {
+      recarga:1380,
+      monto: 1200,
+      programa: "La Banda del Chino",
+      publicidad: ["23:30-23:35", "00:00-00:05"],
+      time: "23:30 - 00:30"
+    }
   ]
-})
+});
+
+// window.database.ref("horarios/domingo").set({
+//   horas: [
+//     {
+//       hora: 05,
+//       publicidad: [
+//         {
+//           intervalo: "5:00-5:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "5:30-5:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 06,
+//       publicidad: [
+//         {
+//           intervalo: "6:00-6:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "6:30-6:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 07,
+//       publicidad: [
+//         {
+//           intervalo: "7:00-7:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "7:30-7:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 08,
+//       publicidad: [
+//         {
+//           intervalo: "8:00-8:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "8:30-8:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 09,
+//       publicidad: [
+//         {
+//           intervalo: "9:00-9:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "9:30-9:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 10,
+//       publicidad: [
+//         {
+//           intervalo: "10:00-10:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "10:30-10:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 11,
+//       publicidad: [
+//         {
+//           intervalo: "11:00-11:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "11:30-11:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 12,
+//       publicidad: [
+//         {
+//           intervalo: "12:00-12:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "12:30-12:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 13,
+//       publicidad: [
+//         {
+//           intervalo: "13:00-13:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "13:30-13:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 14,
+//       publicidad: [
+//         {
+//           intervalo: "14:00-14:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "14:30-14:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 15,
+//       publicidad: [
+//         {
+//           intervalo: "15:00-15:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "15:30-15:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 16,
+//       publicidad: [
+//         {
+//           intervalo: "16:00-16:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "16:30-16:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 17,
+//       publicidad: [
+//         {
+//           intervalo: "17:00-17:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "17:30-17:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 18,
+//       publicidad: [
+//         {
+//           intervalo: "18:00-18:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "18:30-18:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 19,
+//       publicidad: [
+//         {
+//           intervalo: "19:00-19:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "19:30-19:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 20,
+//       publicidad: [
+//         {
+//           intervalo: "20:00-20:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "20:30-20:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 21,
+//       publicidad: [
+//         {
+//           intervalo: "21:00-21:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "21:30-21:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 22,
+//       publicidad: [
+//         {
+//           intervalo: "22:00-22:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "22:30-22:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 23,
+//       publicidad: [
+//         {
+//           intervalo: "23:00-23:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "23:30-23:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 00,
+//       publicidad: [
+//         {
+//           intervalo: "00:00-00:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "00:30-00:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 01,
+//       publicidad: [
+//         {
+//           intervalo: "01:00-01:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "20:30-20:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 02,
+//       publicidad: [
+//         {
+//           intervalo: "02:00-02:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "02:30-02:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 03,
+//       publicidad: [
+//         {
+//           intervalo: "03:00-03:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "03:30-03:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     },
+//     {
+//       hora: 04,
+//       publicidad: [
+//         {
+//           intervalo: "04:00-04:05",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         },
+//         {
+//           intervalo: "04:30-04:35",
+//           duración: 840,
+//           disponibles: 840,
+//           reservados: 0,
+//           status: true
+//         }
+//       ]
+//     }
+//   ]
+// });
