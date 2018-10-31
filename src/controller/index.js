572c765fbd76833369b5197bd1eb0ed10b887443
ticze <ticze2598@gmail.com>
2018-10-31 06:22:46 -0500

    modal con confirmación de reserva

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,118 +1,179 @@
+const btnModal= document.getElementById('btnModal');
+const itemsSelects = [];
 const programa = document.getElementById("programas");
-var starCountRef = firebase.database().ref("programación");
-starCountRef.on("value", function(snapshot) {
-  updateStarCount(snapshot.val());
+btnModal.addEventListener('click',()=>{
+  const arrayItemsReservados = itemsSelects.map((elem)=>`<li class="list-group-item"><div class="d-flex bd-highlight">
+  <div class="p-2 flex-fill bd-highlight">${elem.name}</div>
+  <div class="p-2 flex-fill bd-highlight">${elem.programa}</div>
+  <div class="p-2 flex-fill bd-highlight">${elem.monto}</div>
+</div></li>`)
+const templateReservas= arrayItemsReservados.join('');
+document.getElementById('modalBody').innerHTML ='';
+  document.getElementById('modalBody').innerHTML += `<ul class="list-group">${templateReservas}
+</ul>`;
+})
+const infoUser = {
+  name: 'Marcos Rojas',
+  nameEmpresa: 'Circus',
+  producto: 'Coca Cola',
+}
+
+var programación = firebase.database().ref("programación");
+programación.on("value", function(snapshot) {
+  showDia(snapshot.val());
 });
-const updateStarCount = snap => {
-  const dias = [
-    "lunes",
-    "martes",
-    "miercoles",
-    "jueves",
-    "viernes",
-    "sabado",
-    "domingo"
-  ];
-  dias.forEach(item => {
-    const programming = snap[item].programming;
-    console.log(programming);
-    const arrayTemplateProgramming = programming.map((element, index) => {
-      const sele = element.publicidad;
-      const arrayPubli = sele.map(
-        elem => `<option value="${elem}">${elem}</option>`
-      );
-      const stringOption = arrayPubli.join("");
-      const select = `<select id="intervals">${stringOption}</select>`;
+addInterval =()=>{
+//   let ruta= '';
+  const interval = {
+    name: event.target.value,
+    monto: event.currentTarget.getAttribute("data-monto"),
+    recarga: event.currentTarget.getAttribute("data-recarga"),
+    programa: event.currentTarget.getAttribute("data-programa"),
+    día:event.currentTarget.getAttribute("data-día"),
+  };
+//  firebase.database().ref("programación/"+ interval.día +'/programming').on('value', (snapshot) => {
+//    const _snapshot= snapshot.val()
+//    console.log(_snapshot)
+//    _snapshot.forEach((element, i)=>{element.publicidad.forEach((elem,ind)=>{elem.name === interval.name? ruta = `${i}/publicidad/${ind}` : ruta})});
+//})
+// firebase.database().ref("programación/"+ interval.día +'/programming/' + ruta).update({disponible: 'false', marca: infoUser.producto}) 
+// console.log('ruta',ruta)
+  itemsSelects.push(interval)
+  console.log('items',itemsSelects)}
 
-      return ` <li class="list-group-item " data-dia="${item}" data-index="${index}" data-programa="${
-        element.programa
-      }"  
-      class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" >
-      <div class=" bd-highlight ">
-        <div class="mr-auto bd-highlight ">${element.programa}</div>
-        <div class="bd-highlight">
-          <p class="card-text">
-            <small class="text-muted">${element.time}</small>
-          </p>
+addEventSelect= (elements)=> { 
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('change', addInterval, false);
+  }
+}
+const showDia = snap => {
+  const promesa = new Promise ((resolve, reject)=>{
+    const dias = [
+      "lunes",
+      "martes",
+      "miercoles",
+      "jueves",
+      "viernes",
+      "sabado",
+      "domingo"
+    ];
+    dias.forEach(item => {
+      const programming = snap[item].programming;
+      const arrayTemplateProgramming = programming.map((element, index) => {
+        const sele = element.publicidad;
+        const disponibles = sele.filter(elem=> typeof elem === 'string' || elem.disponible !== 'false')
+        const arrayPubli = disponibles.map(elem => `<option value="${elem.name || elem}">${elem.name || elem}</option>`);
+        const stringOption = arrayPubli.join("");
+        const select = `<select data-programa="${element.programa}" data-día="${item}" data-monto="${element.monto}" data-recarga="${element.recarga || 00}" class="intervals">     
+        <option value="" disabled selected>Seleccione</option>
+        ${stringOption}</select>`;
+        return ` <li class="list-group-item " data-dia="${item}" data-index="${index}" data-programa="${element.programa}"  
+        class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" >
+        <div class=" bd-highlight ">
+          <div class="mr-auto bd-highlight ">${element.programa}</div>
+          <div class="bd-highlight">
+            <p class="card-text">
+              <small class="text-muted">${element.time}</small>
+            </p>
+          </div>
+          <div class="p-2 bd-highlight">
+            <p class="card-text">
+            ${select}
+            </p>
+          </div>
         </div>
-        <div class="p-2 bd-highlight">
-          <p class="card-text">
-          ${select}
-          </p>
-        </div>
-      </div>
-    </li>`;
-    });
-
-    programa.innerHTML += `  <div class="col-12 col-md-2 mt-2 my-0">
-  <div class="row">  
-  <div class="col-12 p-0 bg-dark text-white text-center  border-r">
-  <h3 class=""> ${item}</h3>
-  </div>
-  </div>
-  <div class="row">
-    <div class="col-12 p-0">
-    <ul class="list-group">${arrayTemplateProgramming.join("")} 
-  </ul>
-   </div>
-   </div>
-   </div>`;
-  });
+      </li>`;
+      });
+      programa.innerHTML += `<div class="col-12 col-md-2 mt-2 my-0">
+    <div class="row">  
+    <div class="col-12 p-0 bg-dark text-white text-center  border-r">
+    <h3 class=""> ${item}</h3>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-12 p-0">
+      <ul class="list-group">${arrayTemplateProgramming.join("")} 
+    </ul>
+     </div>
+     </div>
+     </div>`;
+    }, resolve(document.getElementsByClassName('intervals')));
+  })
+  promesa.then((res)=> addEventSelect(res))
 };
 
-const getProgram = () => {
-  const day = event.currentTarget.getAttribute("data-dia");
-  const program = event.currentTarget.getAttribute("data-programa");
-  const index = event.currentTarget.getAttribute("data-index");
-  // console.log(day , program);
-  var starCountRef = firebase.database().ref("programación/" + day);
-  starCountRef.on("value", function(snapshot) {
-    const progra = snapshot.val().programming;
-    const selectprogram = progra.filter(
-      (element, i) => element.programa === program && i == index
-    );
-    showmodal(selectprogram);
-    // console.log(selectprogram);
-  });
-};
+// const getProgram = () => {
+//   const day = event.currentTarget.getAttribute("data-dia");
+//   const program = event.currentTarget.getAttribute("data-programa");
+//   const index = event.currentTarget.getAttribute("data-index");
+//   // console.log(day , program);
+//   var starCountRef = firebase.database().ref("programación/" + day);
+//   starCountRef.on("value", function(snapshot) {
+//     const progra = snapshot.val().programming;
+//     const selectprogram = progra.filter(
+//       (element, i) => element.programa === program && i == index
+//     );
+//     showmodal(selectprogram);
+//     // console.log(selectprogram);
+//   });
+// };
 
-const showmodal = selectprogram => {
-  console.log(selectprogram);
-  const sele = selectprogram[0].publicidad;
-  const arrayPubli = sele.map(
-    elem => `<option value="${elem}">${elem}</option>`
-  );
-  const stringOption = arrayPubli.join("");
-  const select = `<select id="intervals">${stringOption}</select>`;
-  document.getElementById("body").innerHTML = "";
-  document.getElementById(
-    "body"
-  ).innerHTML += ` <div class="modal fade show" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"  >
-<div class="modal-dialog" role="document">
-  <div class="modal-content">
-    <div class="modal-header">
-      <h5 class="modal-title" id="exampleModalLabel">${
-        selectprogram[0].programa
-      }</h5>
-      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body" id="modalBody">
-    <div>   
-    <p>Espacios Disponibles</p>
-     ${select}
-     <p>${selectprogram[0].monto} </p>
-  </div>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-primary">Save changes</button>
-    </div>
-  </div>
-</div>
-</div>`;
-  // , document.getElementById().innerHTML += select;
-};
+// const showmodal = selectprogram => {
+//   console.log(selectprogram);
+//   const sele = selectprogram[0].publicidad;
+//   const arrayPubli = sele.map(
+//     elem => `<option value="${elem}">${elem}</option>`
+//   );
+//   const stringOption = arrayPubli.join("");
+//   const select = `<select class="interval">${stringOption}</select>`;
+//   // console.log(document.getElementsByClassName('.interval'))
+//   document.getElementById("body").innerHTML = "";
+//   document.getElementById(
+//     "body"
+//   ).innerHTML += ` <div class="modal fade show" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"  >
+// <div class="modal-dialog" role="document">
+//   <div class="modal-content">
+//     <div class="modal-header">
+//       <h5 class="modal-title" id="exampleModalLabel">${
+//         selectprogram[0].programa
+//       }</h5>
+//       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//         <span aria-hidden="true">&times;</span>
+//       </button>
+//     </div>
+//     <div class="modal-body" id="modalBody">
+//     <div>   
+//     <p>Espacios Disponibles</p>
+//      ${select}
+//      <p>${selectprogram[0].monto} </p>
+//   </div>
+//     </div>
+//     <div class="modal-footer">
+//       <button type="button" class="btn btn-primary">Save changes</button>
+//     </div>
+//   </div>
+// </div>
+// </div>`;
+//   console.log(".interval");
+//   console.log(document.getElementsByClassName(".interval")); // , document.getElementById().innerHTML += select;
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const btnReserva = document.getElementById("btnreserva");
 // const formulario = document.getElementById("formulario");
--- src/model/firebase-config.js
+++ src/model/firebase-config.js
@@ -1,17 +1,201 @@
-// window.database.ref('horarios').set({
-//   horas: [
-//     {
-//       hora: 05,
-//       publicidad: [
-//         {
-//           intervalo: '5:16-5:30',
-//           duración: 840,
-//           disponibles: 840,
-//           reservados: 0,
-//           status: 'disponible'
-//         },
-//        ],
-//     },
-    
-//   ]
-// })
+window.database.ref("programación/lunes/").set({
+  programming: [
+    {
+      monto: 1200,
+      programa: "Mujeres de Negro",
+      publicidad: [
+        {
+          name: "0030-00:35",
+          monto: 1200,
+          recaga: 1380,
+          programa: "Mujeres de Negro",
+          día: "lunes",
+          fecha: "05/11",
+          disponible: true
+        },
+        {
+          name: "1:00-1:05",
+          monto: 1200,
+          recaga: 1380,
+          programa: "Mujeres de Negro",
+          día: "lunes",
+          fecha: "05/11",
+          disponible: true
+        }
+      ],
+      recarga: 1380,
+      time: "00:30 - 01:30"
+    },
+    {
+      monto: 1000,
+      programa: "Sin tu Mirada",
+      publicidad: [
+        {
+          name: "1:30-1:35",
+          monto: 1000,
+          recaga: 1150,
+          programa: "Sin tu Mirada",
+          día: "05/11",
+          disponible: true
+        },
+        {
+          name: "2:00-2:05",
+          monto: 1000,
+          recaga: 1150,
+          programa: "Sin tu Mirada",
+          día: "05/11",
+          disponible: true
+        }
+      ],
+      recarga: 1150,
+      time: "01:30 - 02:30"
+    },
+    {
+      monto: 3000,
+      programa: "En Boca de Todos (repeticion)",
+      publicidad: ["2:30-2:35", "3:00-3:05"],
+      recarga: 3450,
+      time: "02:30 - 03:30"
+    },
+    {
+      monto: 1000,
+      programa: "El Privilegio de Amar",
+      publicidad: ["3:30-3:35", "4:00-4:05"],
+      recarga: 1150,
+      time: "03:30 - 04:30"
+    },
+    {
+      monto: 2500,
+      programa: "América Espectáculos (repeticion)",
+      publicidad: ["4:30-4:35", "5:00-5:05"],
+      recarga: 2875,
+      time: "04:30 - 05:30"
+    },
+    {
+      monto: 4000,
+      programa: "America Noticias: Primera Edición: Local",
+      publicidad: ["5:30-5:35", "6:00-6:05", "6:30-6:35"],
+      recarga: 4600,
+      time: "5:30 - 07:00"
+    },
+    {
+      monto: 4000,
+      programa: "América Noticias Primera Edición",
+      publicidad: ["7:00-5:05", "7:30-7:35"],
+      recarga: 4600,
+      time: "07:00 - 08:00"
+    },
+    {
+      monto: 1000,
+      programa: "América Deportes",
+      publicidad: ["8:00-8:05"],
+      recarga: 1000,
+      time: "08:00 - 08:20"
+    },
+    {
+      monto: 4000,
+      programa: "América Noticias Primera Edición",
+      publicidad: ["8:30-8:35"],
+      recarga: 4000,
+      time: "08:20 - 09:00"
+    },
+    {
+      monto: 1000,
+      programa: "El Privilegio de Amar",
+      publicidad: ["9:00-9:05", "9:30-9:35"],
+      recarga: 1000,
+      time: "09:00 - 10:00"
+    },
+    {
+      monto: 1000,
+      programa: "Marimar",
+      publicidad: ["10:00-10:05", "10:30-10:35"],
+      recarga: 1000,
+      time: "10:00 - 11:00"
+    },
+    {
+      monto: 1000,
+      programa: "Los Amores de Polo",
+      publicidad: ["11:00-11:05", "11:30-11:35"],
+      recarga: 1000,
+      time: "11:00 - 12:00"
+    },
+    {
+      monto: 4000,
+      programa: "America Noticias: Edición del Mediodia",
+      publicidad: ["12:00-12:05", "12:30-12:35"],
+      recarga: 4200,
+      time: "12:00 - 01:00"
+    },
+    {
+      monto: 3000,
+      programa: "En Boca de Todos",
+      publicidad: ["13:00-13:05", "13:30-13:35", "14:00-14:05"],
+      recarga: 3150,
+      time: "13:00 - 14:20"
+    },
+    {
+      monto: 2200,
+      programa: "Al Fondo Hay Sitio",
+      publicidad: ["14:30-14:35", "15:00-15:05", "15:30-15:35"],
+      recarga: 2310,
+      time: "14:20 - 16:00"
+    },
+    {
+      monto: 1000,
+      programa: "Caer en Tentación",
+      publicidad: ["16:00-16:05", "16:30-16:35"],
+      recarga: 1150,
+      time: "16:00 - 17:00"
+    },
+    {
+      monto: 1200,
+      programa: "La Rosa de Guadalupe",
+      publicidad: ["17:00-17:05", "17:30-17:35", "18:00-18:05"],
+      recarga: 1380,
+      time: "17:00 - 18:20"
+    },
+    {
+      monto: 1200,
+      programa: "Cumbia Pop",
+      publicidad: ["18:30-18:35"],
+      recarga: 1380,
+      time: "18:20 - 19:00"
+    },
+    {
+      monto: 5000,
+      programa: "Esto es Guerra",
+      publicidad: ["19:00-19:05", "19:30-19:35"],
+      recarga: 5750,
+      time: "19:00 - 20:00"
+    },
+    {
+      monto: 1200,
+      programa: "VBQ: Empezando a Vivir",
+      publicidad: ["20:00-20:05", "20:30-20:35"],
+      recarga: 1380,
+      time: "20:00 - 21:00"
+    },
+    {
+      monto: 1200,
+      programa: "Colorina",
+      publicidad: ["21:00-21:05", "21:30-21:35"],
+      recarga: 1380,
+      time: "21:00 - 22:00"
+    },
+    {
+      monto: 4000,
+      programa: "America Noticias: Edicion Central",
+      publicidad: ["22:00-22:05", "22:30-22:35", "23:00-23:05"],
+      recarga: 4600,
+      time: "22:00 - 23:30"
+    },
+    {
+      monto: 1200,
+      programa: "La Banda del Chino",
+      publicidad: ["23:30-23:35", "00:00-00:05"],
+      recarga: 1380,
+      time: "23:30 - 00:30"
+    }
+  ]
+});
